0558beae2c9cc34e3c826b467ef1b091111afa43
HeloiseFerla <ferla.heloise@gmail.com>
2021-10-05 14:01:18 +0200

    Modified Home

--- script.js
+++ script.js
@@ -11,10 +11,11 @@ crossIcon.addEventListener("click",function(){
 
 let slider = document.querySelector(".slider");
 let slides = document.querySelectorAll(".slide");
-//interval
+
 let interval = window.setInterval(slide,1000);
 //slide
 function slide(){
+    
     let imgSize = slides[0].width;
     slider.style.transform = 'translateX($imgSize + "px")';
     slider.appendChild(slides[0]);
